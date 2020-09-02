8fed714023e7234eccc6f6764e378b7612258f59
mirat432 <68599680+mirat432@users.noreply.github.com>
2020-09-02 16:25:27 +0000

    Uploading Assignment

---
+++ HW3_6JWF8L9X5T/src/components/NavbarComponents.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+
+class NavbarComponents extends Component {
+    render() {
+        return(
+           
+        <div>
+            <nav className="navbar">
+            <div className="container">
+            <div  href="#">Hulk</div>
+            <div className="logo" href="#" >Mavel</div>
+            <div href="#">Venom</div>
+            </div>
+            </nav>
+            </div>
+            
+        );  
+    }
+}
+export default NavbarComponents
